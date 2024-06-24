853c40e3fbfde54577a5032c774d500a9e9051d3
subha152 <subho.3600.ghosh@gmail.com>
2024-06-24 09:51:25 +0530

    This is First commit

---
+++ src/main/java/com/demo/application/NewprojectApplication.java
@@ -0,0 +1,15 @@
+package com.demo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprojectApplication.class, args);
+		System.out.println("Hi I am SpringBoot");	
+		
+	}
+
+}
